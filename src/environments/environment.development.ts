53ef800436a8bc14a1a133cd7a6502ac8ebe94d1
Douglas Carneiro <douglas.ul@gmail.com>
2025-05-25 15:34:54 -0300

    feat: adicionando dotenv

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 import encKey from './encryptionKey';
 
+declare var process: any;
+
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
@@ -10,12 +12,12 @@ export const environment = {
 
   // Firebase
   firebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: "V"
+    apiKey: process.env["NG_APP_APIKEY"],
+    authDomain: process.env["NG_APP_AUTHDOMAIN"],
+    projectId: process.env["NG_APP_PROJECTID"],
+    storageBucket: process.env["NG_APP_STORAGEBUCKET"],
+    messagingSenderId: process.env["NG_APP_MESSAGINGSENDERID"],
+    appId: process.env["NG_APP_APPID"],
+    measurementId: process.env["NG_APP_MEASUREMENTID"],
   }
 };
